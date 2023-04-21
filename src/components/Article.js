d1a4c05a84f2792a23ceb993d1049289d89cb7f2
Blake <BlakeKDev@gmail.com>
2023-04-20 19:42:25 -0500

    First Commit

---
+++ src/components/Article.js
@@ -0,0 +1,32 @@
+export default function Article () {
+    return (
+        <>
+        <main>
+            <article>
+                <time datetime="2020-11-12">12 Novermber 2020</time>
+                <h2 classNames="article-title">On the Street in Brooklyn</h2>
+                <img src="blog-image-1.jpg" alt="blog-image-1"/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero natus libero sit fugit nam consectetur, impedit unde veniam dolores saepe voluptatum earum enim labore minima totam amet nihil praesentium repellat.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quisquam et deserunt odio ipsam, quos, doloremque corporis nostrum rerum nisi suscipit optio dicta beatae! In commodi quos voluptatum porro vel!Lorem
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis voluptates, explicabo accusamus similique quidem ex et aut sunt ipsa dolor amet perspiciatis porro eius fugiat dolorum ullam magnam ut!
+                </p>
+                <aside>
+                <h3> <a href="#" class="cont">Continues...</a></h3>
+                </aside>
+            </article>
+            <article>
+                <time datetime="2020-11-11">11 Novermber 2020</time>
+                <h2 classNames="article-title">Vintage in Vogue</h2>
+                <img src="blog-image-2.jpg" alt="blog-image-2"/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero natus libero sit fugit nam consectetur, impedit unde veniam dolores saepe voluptatum earum enim labore minima totam amet nihil praesentium repellat.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quisquam et deserunt odio ipsam, quos, doloremque corporis nostrum rerum nisi suscipit optio dicta beatae! In commodi quos voluptatum porro vel!Lorem
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis voluptates, explicabo accusamus similique quidem ex et aut sunt ipsa dolor amet perspiciatis porro eius fugiat dolorum ullam magnam ut!
+                </p>
+                <aside>
+                <h3> <a href="#" class="cont">Continues...</a></h3>
+                </aside>
+            </article>
+        </main>
+        </>
+    )
+    }
